7202afb21ae008077409355727efa038e0b41f5a
Chetan Shanbag <chetan.shanbag@bazaarvoice.com>
2023-02-20 22:25:07 +0530

    Built Calcilator with Banners andinterstitial added

---
+++ app/src/main/java/com/example/mycalculator/BannerFragment.java
@@ -0,0 +1,68 @@
+package com.example.mycalculator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.pubnative.lite.sdk.interstitial.HyBidInterstitialAd;
+import net.pubnative.lite.sdk.views.HyBidBannerAdView;
+import net.pubnative.lite.sdk.views.PNAdView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+public class BannerFragment extends Fragment {
+
+    private HyBidBannerAdView mBanner;
+    private HyBidInterstitialAd mInterstitial;
+    String Zone_Id = "3";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_banner, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBanner = view.findViewById(R.id.hybid_banner);
+        loadInterstitial();
+    }
+    private void loadInterstitial() {
+        mInterstitial = new HyBidInterstitialAd(getActivity(),Zone_Id, new HyBidInterstitialAd.Listener() {
+            @Override
+            public void onInterstitialLoaded() {
+                mInterstitial.show();
+            }
+
+            @Override
+            public void onInterstitialLoadFailed(Throwable error) {
+
+            }
+
+            @Override
+            public void onInterstitialImpression() {
+
+            }
+
+            @Override
+            public void onInterstitialDismissed() {
+
+            }
+
+            @Override
+            public void onInterstitialClick() {
+
+            }
+        });
+        mInterstitial.load();
+    }
+}
---
+++ app/src/main/java/com/example/mycalculator/InterstitialFragment.java
@@ -0,0 +1,60 @@
+package com.example.mycalculator;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import net.pubnative.lite.sdk.views.HyBidBannerAdView;
+import net.pubnative.lite.sdk.views.PNAdView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the  factory method to
+ * create an instance of this fragment.
+ */
+public class InterstitialFragment extends Fragment {
+    String Zone_Id = "2";
+    private HyBidBannerAdView mBanner;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_interstitial, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBanner = view.findViewById(R.id.hybid_banner);
+
+
+         loadBanner();
+    }
+
+    private void loadBanner() {
+        mBanner.load(Zone_Id, new PNAdView.Listener() {
+            @Override
+            public void onAdLoaded() {
+
+            }
+
+            @Override
+            public void onAdLoadFailed(Throwable error) {
+
+            }
+
+            @Override
+            public void onAdImpression() {
+
+            }
+
+            @Override
+            public void onAdClick() {
+
+            }
+        });
+    }
+}
